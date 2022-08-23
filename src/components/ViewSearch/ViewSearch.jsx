202aea7cfbeb789ecd1a517d3d3b730fa61c2055
Juan Agustin Alejandro <alejandro4juan@gmail.com>
2022-08-22 21:45:52 -0300

    Feature:\GeoLocation

--- src/components/ViewSearch/ViewSearch.jsx
+++ src/components/ViewSearch/ViewSearch.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import useFetchAxios from '../../helpers/useFetchAxios';
+import useAxios from '../../helpers/useAxios';
 import { useClimaContex } from '../../providers/ClimaProvider';
 import { Card } from '../Card';
 
 const ViewSearch = ({ flagSearch }) => {
   const location = useClimaContex();
-  const dataclima = useFetchAxios('https://api.weatherapi.com/v1/current.json?key=6be8c28794924ed8a2a184922222905&q=', location);
-  return <>
-  <button className='btn btn-primary' onClick={flagSearch}>Search City</button>
-  {!dataclima.loading && <Card temp={dataclima.data.current.temp_c}
-  time={dataclima.data.location.localtime}
-  region={dataclima.data.location.region}
-  name={dataclima.data.location.name}
-  wind_kph={dataclima.data.current.wind_kph}
-  humidity={dataclima.data.current.humidity}
-  icon={dataclima.data.current.condition.icon}
-  />}
-  </>;
+  const dataclima = useAxios('https://api.weatherapi.com/v1/current.json?key=6be8c28794924ed8a2a184922222905&q=', location);
+  return (
+    <>
+      <button className="btn btn-primary" onClick={flagSearch}>
+        Search City
+      </button>
+      {!dataclima.loading && (
+        <Card
+          temp={dataclima.data.current.temp_c}
+          time={dataclima.data.location.localtime}
+          region={dataclima.data.location.region}
+          name={dataclima.data.location.name}
+          wind_kph={dataclima.data.current.wind_kph}
+          humidity={dataclima.data.current.humidity}
+          icon={dataclima.data.current.condition.icon}
+        />
+      )}
+    </>
+  );
 };
 
 export default ViewSearch;
